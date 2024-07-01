8e3bdf00786702fdcebebbba311b3c97aa079503
Devayzer <dvzr.official@gmail.com>
2024-07-01 08:28:25 +0300

    init powerboard

---
+++ Software/PowerBoard/v.0.1/v.0.1.ino
@@ -0,0 +1,14 @@
+#include <Adafruit_SSD1306.h>
+#include <splash.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
